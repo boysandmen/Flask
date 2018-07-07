ca851a0b4c752cb46d1ea286c93c56f036dc594c
R <R>
2018-07-07 17:05:17 +0800

    Flask简单流程走通

---
+++ flask01.py
@@ -0,0 +1,14 @@
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/user/<name>')
+def index(name):
+    return '<h1>Hello %s </h1>' % name
+
+
+if __name__ == '__main__':
+    app.run()
