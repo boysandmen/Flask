6612eb2b61eb4bfa3768f4cbb9f941f51a3d4b2f
R <R>
2018-07-07 17:20:19 +0800

    Flask简单流程走通+ 格式化输出

--- flask01.py
+++ flask01.py
@@ -10,5 +10,13 @@ def index(name):
     return '<h1>Hello %s </h1>' % name
 
 
+from flask import request
+
+
+@app.route('/index')
+def index_too():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
