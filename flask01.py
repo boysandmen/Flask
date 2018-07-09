d10d8273f14c2c249095a517d66b9f12dbff66ba
R <R>
2018-07-09 16:15:25 +0800

    Flask简单流程走通+ 格式化输出+ 返回400的状态码

--- flask01.py
+++ flask01.py
@@ -1,6 +1,5 @@
-
-
 from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -17,6 +16,23 @@ from flask import request
 def index_too():
     user_agent = request.headers.get('User-Agent')
     return '<p>Your browser is %s</p>' % user_agent
+
+
+@app.route('/index_bad')
+def index_bad():
+    return '<h1>Bad Request</h1>', 400
+
+
+from flask import make_response
+
+
+@app.route('/')
+def index_cookie():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
